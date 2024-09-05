dd1ee57307d41d3de7e1194c393661cdd1835e1b
Vetal <heavyve@gmail.com>
2024-09-06 00:08:24 +0300

    add map isSName, isNumber

--- homework-03-collections-map/src/main/java/com/company/PhoneBook.java
+++ homework-03-collections-map/src/main/java/com/company/PhoneBook.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.*;
+import java.util.regex.Pattern;
 
 public class PhoneBook {
 
@@ -19,13 +20,13 @@ public class PhoneBook {
     public Set<String> getContactByName(String name) {
         // TODO формат одного контакта "Имя - Телефон"
         // TODO если контакт не найден - вернуть пустой TreeSet
-        return null;
+        return new TreeSet<>();
     }
 
     public String getContactByPhone(String phone) {
         // TODO формат одного контакта "Имя - Телефон"
         // TODO если контакт не найдены - вернуть пустую строку
-        return null;
+        return "";
     }
 
     public Set<String> getAllContacts() {
@@ -33,5 +34,13 @@ public class PhoneBook {
         // TODO если контактов нет в телефонной книге - вернуть пустой TreeSet
 
         return new TreeSet<>();
+    }
+
+    public boolean isSName(String name){
+        return Pattern.matches("[a-zA-Z]+", name);
+    }
+
+    public boolean isNumber(String phone){
+        return Pattern.matches("\\d+", phone);
     }
 }
