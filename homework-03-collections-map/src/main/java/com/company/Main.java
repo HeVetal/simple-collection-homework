796495c90e742c66ff716d9d447a0e0feecc0c8f
Vetal <heavyve@gmail.com>
2024-09-06 12:29:47 +0300

    clear Main

--- homework-03-collections-map/src/main/java/com/company/Main.java
+++ homework-03-collections-map/src/main/java/com/company/Main.java
@@ -1,29 +1,9 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Main {
-    protected PhoneBook phoneBook;
 
     public static void main(String[] args) {
-        PhoneBook phoneBook = new PhoneBook();
-
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            String in = reader.readLine();
-            if (in.equals("LIST")) {
-                phoneBook.getAllContacts();
-            } else if (phoneBook.isNumber(in)) {
-                phoneBook.getContactByPhone(in);
-            } else if (phoneBook.isSName(in)) {
-                phoneBook.getContactByName(in);
-            } else {
-                System.out.println("Неверный формат ввода");
-            }
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        PhoneBookApp phoneBookApp = new PhoneBookApp();
+        phoneBookApp.init();
     }
 }
