fe83418724908cc5d6389f243e29a3826e8adc3a
Vetal <heavyve@gmail.com>
2024-09-23 17:03:51 +0300

    add writeMessage, readString

--- homework-03-collections-map/src/main/java/com/company/ConsoleHelper.java
+++ homework-03-collections-map/src/main/java/com/company/ConsoleHelper.java
@@ -1,9 +1,28 @@
 package com.company;
 
 
+import javax.imageio.IIOException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
 
+    public static String readString(){
+        String string;
+        try{
+            string = bufferedReader.readLine();
+        } catch (IOException e){
+            writeMessage("Произошла ошибка при попытке ввода текста, попробуйте еще раз");
+            string = readString();
+        }
+        return string;
+    }
 }
 
 
