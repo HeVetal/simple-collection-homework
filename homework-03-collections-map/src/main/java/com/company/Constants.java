7e6bb340e97ea8dd2508f60390cb16fd828b63b8
HeVetal <heavyve@gmail.com>
2024-09-25 14:34:23 +0300

    add Constants

--- homework-03-collections-map/src/main/java/com/company/Constants.java
+++ homework-03-collections-map/src/main/java/com/company/Constants.java
@@ -2,4 +2,11 @@ package com.company;
 
 public interface Constants {
     public static final String LIST = "LIST";
+    String ENTER_NAME_PHONE_COMMAND = "Введите номер, имя или команду:";
+    String NOT_NAME_IN_BOOK = "Такого имени в телефонной книге нет.";
+    String ENTER_PHONE = "Введите номер телефона для абонента “";
+    String NOT_PHONE_IN_BOOK = "Такого номера нет в телефонной книге.";
+    String ENTER_NAME = "Введите имя абонента для номера “";
+    String SAVE_CONTACT = "Контакт сохранен!";
+    String WRONG_FORMAT = "Неверный формат ввода";
 }
