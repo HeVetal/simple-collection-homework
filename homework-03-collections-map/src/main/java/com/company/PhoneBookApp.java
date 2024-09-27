e542f40146f65512e187115ec25b09a4be3b534e
HeVetal <heavyve@gmail.com>
2024-09-27 15:02:49 +0300

    add refactor

--- homework-03-collections-map/src/main/java/com/company/PhoneBookApp.java
+++ homework-03-collections-map/src/main/java/com/company/PhoneBookApp.java
@@ -1,51 +1,56 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Set;
-import java.util.TreeSet;
+import static com.company.ConsoleHelper.*;
+import static com.company.Constants.*;
 
 public class PhoneBookApp {
     public void init() {
         PhoneBook phoneBook = new PhoneBook();
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            while (true) {
-                ConsoleHelper.writeMessage(Constants.ENTER_NAME_PHONE_COMMAND);
-                String in = reader.readLine();
-                if (in.equals("LIST")) {
-                    for (String contact : phoneBook.getAllContacts()) {
-                        ConsoleHelper.writeMessage(contact);
-                    }
-                    ConsoleHelper.writeMessage(System.lineSeparator());
-                } else if (phoneBook.isPhone(in)) {
-                    if (phoneBook.getContactByPhone(in).isEmpty()) {
-                        ConsoleHelper.writeMessage(Constants.NOT_PHONE_IN_BOOK);
-                        ConsoleHelper.writeMessage(Constants.ENTER_NAME + in + "”:");
-                        phoneBook.addContact(ConsoleHelper.readString(), in);
-                        ConsoleHelper.writeMessage(Constants.SAVE_CONTACT + System.lineSeparator());
-                    } else {
-                        ConsoleHelper.writeMessage(phoneBook.getContactByPhone(in) + System.lineSeparator());
-                    }
-                } else if (phoneBook.isSName(in)) {
-                    if (phoneBook.getContactByName(in).equals(new TreeSet<>())) {
-                        ConsoleHelper.writeMessage(Constants.NOT_NAME_IN_BOOK);
-                        ConsoleHelper.writeMessage(Constants.ENTER_PHONE + in + "”:");
-                        phoneBook.addContact(in, ConsoleHelper.readString());
-                        ConsoleHelper.writeMessage(Constants.SAVE_CONTACT + System.lineSeparator());
-                    } else {
-                        for (String name : phoneBook.getContactByName(in)) {
-                            ConsoleHelper.writeMessage(name);
-                        }
-                    }
-                    ConsoleHelper.writeMessage(System.lineSeparator());
-                } else {
-                    ConsoleHelper.writeMessage(Constants.WRONG_FORMAT);
+        boolean isExit = false;
+        while (!isExit) {
+            writeMessage(ENTER_NAME_PHONE_COMMAND);
+            String in = readString();
+            if (in.equals("LIST")) {
+                for (String contact : phoneBook.getAllContacts()) {
+                    writeMessage(contact);
                 }
-            }
+                writeMessage(System.lineSeparator());
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            } else if (phoneBook.isSName(in)) {
+                if (!phoneBook.getMap().containsKey(in)) {
+                    writeMessage(NOT_NAME_IN_BOOK);
+                    writeMessage(ENTER_PHONE + in + "”:");
+                    String phone = readString();
+                    if (phoneBook.isPhone(phone)) {
+                        phoneBook.addContact(in, phone);
+                        writeMessage(SAVE_CONTACT + System.lineSeparator());
+                    } else {
+                        writeMessage(WRONG_FORMAT);
+                    }
+                } else {
+                    for (String name : phoneBook.getContactByName(in)) {
+                        writeMessage(name);
+                    }
+                }
+                writeMessage(System.lineSeparator());
+            } else if (phoneBook.isPhone(in)) {
+                if (phoneBook.getContactByPhone(in).isEmpty()) {
+                    writeMessage(NOT_PHONE_IN_BOOK);
+                    writeMessage(ENTER_NAME + in + "”:");
+                    String name = readString();
+                    if (phoneBook.isSName(name)) {
+                        phoneBook.addContact(name, in);
+                        writeMessage(SAVE_CONTACT + System.lineSeparator());
+                    } else {
+                        writeMessage(WRONG_FORMAT);
+                    }
+                } else {
+                    writeMessage(phoneBook.getContactByPhone(in) + System.lineSeparator());
+                }
+            } else {
+                writeMessage(WRONG_FORMAT);
+                isExit = true;
+            }
         }
     }
 }
