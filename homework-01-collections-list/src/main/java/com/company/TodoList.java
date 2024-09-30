d91ffc73a508e6eac76a12aa00677858ef898089
HeVetal <heavyve@gmail.com>
2024-09-30 15:28:48 +0300

    add all methods

--- homework-01-collections-list/src/main/java/com/company/TodoList.java
+++ homework-01-collections-list/src/main/java/com/company/TodoList.java
@@ -1,30 +1,47 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class TodoList {
+    private List<String> list = new ArrayList<>();
 
     public void add(String todo) {
         // TODO: добавьте переданное дело в конец списка
+        list.add(todo);
     }
 
     public void add(int index, String todo) {
         // TODO: добавьте дело на указанный индекс,
         //  проверьте возможность добавления
+        if (checkIndex(index)) {
+            list.add(index, todo);
+        }
     }
 
     public void edit(int index, String todo) {
         // TODO: заменить дело на index переданным todo индекс,
         //  проверьте возможность изменения
+        if (checkIndex(index)) {
+            list.set(index, todo);
+        }
     }
 
     public void delete(int index) {
         // TODO: удалить дело находящееся по переданному индексу,
         //  проверьте возможность удаления дела
+        if (checkIndex(index)) {
+            list.remove(index);
+        }
     }
 
     public ArrayList<String> getTodos() {
         // TODO: вернуть список дел
-        return null;
+        return new ArrayList<>(list);
     }
+
+    private boolean checkIndex(int index) {
+        return index > 0 && index < list.size() - 1;
+    }
+
 }
